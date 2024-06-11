1cd8713a63ef0d295ec8209cfddb1101024f3931
Claudio Nave <evaristegalois11@proton.me>
2024-06-11 23:20:36 +0200

    Init project

---
+++ src/main/java/org/example/pagemodelswagger/PageModelSwaggerApplication.java
@@ -0,0 +1,13 @@
+package org.example.pagemodelswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageModelSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PageModelSwaggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pagemodelswagger/PageModelSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pagemodelswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageModelSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
